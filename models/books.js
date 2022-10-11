c5ac88516221081177435b6bc533fa835c4f45c1
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-12 00:11:26 +0100

    jwt

--- models/books.js
+++ models/books.js
@@ -1,42 +1,47 @@
-const moogoose = require('mongoose');
+const moogoose = require("mongoose");
 
 //Define a schema
 const Schema = moogoose.Schema;
 
 //Define book schema
-const BookSchema = new Schema({
+const BookSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     shortDescription: {
-        type: String,
-        required: false
+      type: String,
+      required: false
     },
     year: {
-        type: Number,
-        required: true,
-        max: [2022, 'Year must be less than or equal to 2020'] //validation with custom message
+      type: Number,
+      required: true,
+      max: [2022, "Year must be less than or equal to 2020"] //validation with custom message
     },
     isbn: {
-        type: String,
-        required: true,
-        unique: [true, 'ISBN must be unique'] //validation with custom message
+      type: String,
+      required: true,
+      unique: [true, "ISBN must be unique"] //validation with custom message
     },
     price: {
-        type: Number,
-        required: true,
-        min: [0, 'Price must be greater than or equal to 0'] //validation with custom message
+      type: Number,
+      required: true,
+      min: [0, "Price must be greater than or equal to 0"] //validation with custom message
     },
-    createAt : {
-        type: Date,
-        default: Date.now
+    createAt: {
+      type: Date,
+      default: Date.now
     },
-    lastUpdateAt : {
-        type: Date,
-        default: Date.now
-    },
-});
+    lastUpdateAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 // Export the model
-module.exports = moogoose.model('Books', BookSchema); //collection name is Books. This is the name of the collection in the database
+module.exports = moogoose.model("Books", BookSchema); //collection name is Books. This is the name of the collection in the database
